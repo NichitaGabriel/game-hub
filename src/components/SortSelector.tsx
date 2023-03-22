17039d2a12a3234d8a9e53035e26610b437f3665
NichitaGabriel <gabinichita1999@gmail.com>
2023-03-22 13:08:29 +0200

    Fixed a word in sort list

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -13,7 +13,7 @@ const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
 		{ value: "name", label: "Name" },
 		{ value: "-released", label: "Release date" },
 		{ value: "-metacritic", label: "Popularity" },
-		{ value: "-rating", label: "average rating" },
+		{ value: "-rating", label: "Average rating" },
 	];
 
 	const currentSortOrder = sortOrders.find((order) => order.value === sortOrder);
